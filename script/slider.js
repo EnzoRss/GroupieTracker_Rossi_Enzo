dd029ae4ad02127f494aa9ee1c6ff22cda2b57c2
EnzoRss <enzo.rossi@ynov.com>
2023-03-11 17:27:39 +0100

    index and header

--- script/slider.js
+++ script/slider.js
@@ -2,7 +2,7 @@ let container =document.getElementById("container");
 let g =document.getElementById("g")
 let d =document.getElementById("d");
 let p=0;
-let nbr= 5;
+let nbr= 4;
 function afficherMasquer(){
     if(p ===-nbr+1)
         d.style.visibility="hidden";
@@ -34,14 +34,14 @@ document.body.onload =function (){
     }
 }
 
-// setInterval(Slider,100000)
-// function Slider(){
-//     if(p === -nbr+1)
-//         p = 1
-//     p--;
-//     container.style.transform="translate("+p*100+"vw)";
-//     container.style.transition ="all 0.5s ease"
-//     console.log(p)
-// }
+setInterval(Slider,100000)
+function Slider(){
+    if(p === -nbr+1)
+        p = 1
+    p--;
+    container.style.transform="translate("+p*100+"vw)";
+    container.style.transition ="all 0.5s ease"
+    console.log(p)
+}
 
 
